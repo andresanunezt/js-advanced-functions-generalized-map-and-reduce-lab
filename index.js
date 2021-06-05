f681905e7ea370220970621a257ba4b365a6e995
Andrés Nuñez <andresanunez98@gmail.com>
2021-06-05 16:12:21 -0400

    commit tree a9b2a8b7df901ae689da325cffef9c6e46a92abf

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Add your functions here
+// const map = function(sourceArray,callBack){
+
+// };
+
+function map(sourceArray, callBack) {
+    let newArray = []
+  
+    for (let i = 0; i < sourceArray.length; i++) {
+      let theElement = sourceArray[i]
+      newArray.push(callBack(theElement))
+    }
+  
+    return newArray;
+  }
+
+
+  function reduce(sourceArray, callBack, startingPoint ) {
+    
+      let total;
+
+      if(startingPoint){
+          total = startingPoint;
+          for (let i = 0; i < sourceArray.length; i++) {
+              total = callBack(sourceArray[i], total);
+          }
+      }
+
+      else {
+        total = sourceArray[0];
+        for (let i = 1; i < sourceArray.length; i++) {
+          total = callBack(sourceArray[i], total);
+      }
+      }
+
+      return total;
+  }
